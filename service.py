01fbc887f5eedc164957f68f148f6c7fde3d1272
marians <marian@giantswarm.io>
2017-01-11 12:15:08 +0100

    Fixing k8s API auth

--- service.py
+++ service.py
@@ -33,10 +33,7 @@ def archive_event(event):
         index=index,
         id=event_hash(event))
     data = {"_id": id, "_source": event}
-    headers = {
-        "Authorization": "Bearer %s" % token
-    }
-    r = requests.put(url, headers=headers, json=event)
+    r = requests.put(url, json=event)
     if r.status_code == 201:
         print("Archived event %s" % url)
     else:
@@ -50,10 +47,14 @@ def event_hash(event):
 if __name__ == "__main__":
     signal.signal(signal.SIGTERM, sigterm_handler)
     TOKEN = get_default_token()
+    headers = {
+        "Authorization": "Bearer %s" % TOKEN
+    }
+
     time.sleep(10)
 
     while True:
-        r = requests.get(KUBERNETES_API_URL, stream=True, verify=False)
+        r = requests.get(KUBERNETES_API_URL, headers=headers, stream=True, verify=False)
         if r.encoding is None:
             r.encoding = 'utf-8'
         for line in r.iter_lines(decode_unicode=True):
